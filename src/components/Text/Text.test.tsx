d31bdf0c24d48fe0a9615295bdd000df9f96a8cf
gdiaz2 <150080639+gdiaz2@users.noreply.github.com>
2024-07-03 10:16:02 -0500

    Run all tests

--- src/components/Text/Text.test.tsx
+++ src/components/Text/Text.test.tsx
@@ -5,7 +5,7 @@ import { Text } from './Text';
 
 describe('Text Component', () => {
   test('renders Medium Text', () => {
-    render(<Text text="Medium Text" size="small" visible={true} bold={false} disabled={false} fontColor="black" />);
+    render(<Text text="Medium Text" size="medium" visible={true} bold={false} disabled={false} fontColor="black" />);
     const textElement = screen.getByText('Medium Text');
     expect(textElement).toBeInTheDocument();
     expect(textElement).toHaveStyle('font-size: 16px');
